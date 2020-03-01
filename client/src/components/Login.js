720d9ea08bcf1a29f503f9630829d01a3913f147
Densovan <sarimramata@gmail.com>
2020-03-01 20:21:53 +0700

    add frond end

---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+const Login = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default Login;
